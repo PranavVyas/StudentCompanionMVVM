66ee2f94bd647bd21ef301e6ff7261c472f12082
Pranav Vyas <pranavvyas4399@gmail.com>
2019-06-03 19:17:26 +0530

    *Add Fetch dependency *Start working on download option
    Signed-off-by: Pranav Vyas <pranavvyas4399@gmail.com>

--- app/src/main/java/com/vyas/pranav/studentcompanion/adapters/DigitalLibraryRecyclerAdapter.java
+++ app/src/main/java/com/vyas/pranav/studentcompanion/adapters/DigitalLibraryRecyclerAdapter.java
@@ -1,11 +1,20 @@
 package com.vyas.pranav.studentcompanion.adapters;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.vyas.pranav.studentcompanion.R;
 import com.vyas.pranav.studentcompanion.data.digitallibrarydatabase.DigitalLibraryEntry;
 
@@ -35,6 +44,47 @@ public class DigitalLibraryRecyclerAdapter extends RecyclerView.Adapter<DigitalL
         holder.tvSubject.setText(book.getSubject());
         holder.tvNo.setText((position + 1) + ".");
         holder.tvExtraInfo.setText(book.getExtraInfo());
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkForPermission(holder.itemView.getContext(), book.getBookUrl());
+            }
+        };
+
+        holder.itemView.setOnClickListener(clickListener);
+    }
+
+    private void checkForPermission(Context context, String downloadUrl) {
+        Dexter.withActivity((Activity) context)
+                .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(context, "Thank you for permission", Toast.LENGTH_SHORT).show();
+                            startDownloadDocument(downloadUrl);
+                        } else {
+                            String deniedPermissioString = "";
+                            List<PermissionDeniedResponse> deniedPermissionResponses = report.getDeniedPermissionResponses();
+                            for (PermissionDeniedResponse res :
+                                    deniedPermissionResponses) {
+                                deniedPermissioString += res.getPermissionName();
+                            }
+                            Toast.makeText(context, "Permissions " + deniedPermissioString + " are required!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void startDownloadDocument(String downloadUrl) {
+
     }
 
     @Override
@@ -47,7 +97,7 @@ public class DigitalLibraryRecyclerAdapter extends RecyclerView.Adapter<DigitalL
         notifyDataSetChanged();
     }
 
-    class BookHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class BookHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.tv_recycler_digital_library_book_holder_book_name)
         TextView tvBookName;
         @BindView(R.id.tv_recycler_digital_library_book_holder_author_name)
@@ -62,12 +112,6 @@ public class DigitalLibraryRecyclerAdapter extends RecyclerView.Adapter<DigitalL
         BookHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(v.getContext(), "Link is : " + listOfBooks.get(getAdapterPosition()).getBookUrl(), Toast.LENGTH_SHORT).show();
         }
     }
 }
